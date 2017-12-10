4ecdad339a681d7ce373899aa7c3614d050228d7
Jamie Sime <jamiesime@gmail.com>
2017-12-10 16:10:27 +0000

    successfully fetching info from API

--- public/app.js
+++ public/app.js
@@ -1,3 +1,4 @@
+var API_KEY = "d15950aed68c4a24b3a6545c93c59780";
 
 var makeRequest = function(url, callback){
   var request = new XMLHttpRequest();
@@ -10,22 +11,37 @@ var requestComplete = function(){
   if (this.status !== 200) return;
   var jsonString = this.responseText;
   var response = JSON.parse(jsonString);
-  debugger;
+  printHeadlines(response);
 };
 
-var printResponse = function(response){
-  for (item in response){
-    var responseContainer = document.querySelector("#response-area")
-    var responseItem = document.createElement("p");
-    responseContainer.appendChild(responseItem);
+var printHeadlines = function(response){
+  var sourceDiv = document.querySelector('#source-rows');
+  var articleList = response.articles;
+  var headline;
+  var headlineTag;
+  var sourceBox;
+  for (i = 0 ; i < articleList.length ; i++){
+    headline = articleList[i].title;
+    sourceBox = document.createElement("div");
+    sourceBox.className = "source-box";
+    headlineList = document.createElement("ul")
+    headlineTag = document.createElement("li");
+    headlineList.appendChild(headlineTag);
+    headlineTag.innerText = headline;
+
+    sourceBox.appendChild(headlineList);
+    sourceDiv.appendChild(sourceBox);
   }
 }
 
-var app = function(){
-  var url = "http://api.musixmatch.com/ws/1.1/";
+var generateSourceBox = function(source){
+  var url = "https://newsapi.org/v2/top-headlines?sources="+source+"&apiKey=d15950aed68c4a24b3a6545c93c59780";
   makeRequest(url, requestComplete);
+}
 
-
+var app = function(){
+  generateSourceBox("techcrunch");
+  generateSourceBox("bbc-news");
 }
 
 window.addEventListener('load', app);
